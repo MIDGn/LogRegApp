13964aca1d0401616c9ed4f1ce28cb1cef953a07
Georgy Konovalov <kong_19@mail.ru>
2023-04-23 18:39:16 +0300

    fix

--- web.py
+++ web.py
@@ -20,7 +20,7 @@ nlp = spacy.load('en_core_web_sm')
 
 def preprocess_text(text):
     """ Preprocess input text """
-    text = re.sub('[-,\.!?]', '', text).lower()
+    text = re.sub('[-,\\.!?]', '', text).lower()
     doc = nlp(text)
     lemmas = [token.lemma_ for token in doc if not token.is_stop]
     return " ".join(lemmas)
